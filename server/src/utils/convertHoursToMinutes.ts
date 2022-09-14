9d3e00d1034b1b41432de1aff7fa5f3e81ce45e5
Luan <luancnunes04@gmail.com>
2022-09-14 17:02:11 -0300

    feat: added SQLite, prisma, created base endpoints

---
+++ server/src/utils/convertHoursToMinutes.ts
@@ -0,0 +1,5 @@
+export const convertHoursToMinutes = (hour: string) => {
+    const [hours, minutes] = hour.split(':').map(Number)
+    const minutesAmount = (hours * 60) + minutes
+    return minutesAmount
+}
